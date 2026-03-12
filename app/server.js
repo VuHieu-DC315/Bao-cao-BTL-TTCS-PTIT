1891b90984a3a9a533b23febcf9f48613d38aba2
VuHieu-DC315 <hieubao189@gmail.com>
2026-03-12 15:15:02 +0700

    cap nhat lan 3

--- app/server.js
+++ app/server.js
@@ -3,6 +3,8 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, "./public")));
 // var corsOptions = {
